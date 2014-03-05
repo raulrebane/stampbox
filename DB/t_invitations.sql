07774e52c1f614f8befad2fe4c1920ac2fec696d
Raul Rebane <raulrebane71@gmail.com>
2014-03-05 23:29:03 +0200

    Invitations table modified

--- DB/t_invitations.sql
+++ DB/t_invitations.sql
@@ -1,24 +1,29 @@
--- Table: ds.t_invitations
+-- Table: ds.t_customer_mailbox
 
--- DROP TABLE ds.t_invitations;
+-- DROP TABLE ds.t_customer_mailbox;
 
-CREATE TABLE ds.t_invitations
+CREATE TABLE ds.t_customer_mailbox
 (
   customer_id bigint NOT NULL,
-  invited_email character varying(100) NOT NULL,
-  invited_when timestamp without time zone,
-  from_count integer,
-  to_count integer,
-  CONSTRAINT pk_invitations PRIMARY KEY (customer_id, invited_email),
-  CONSTRAINT fk_invitations_t_customer FOREIGN KEY (customer_id)
+  e_mail character varying(100) NOT NULL,
+  e_mail_username character varying(100),
+  e_mail_password character varying(32),
+  status character(1) NOT NULL,
+  maildomain character varying(100) NOT NULL,
+  worker_ip inet,
+  worker_type character varying(20),
+  last_seen timestamp(6) without time zone,
+  CONSTRAINT pk_customer_mailbox PRIMARY KEY (e_mail),
+  CONSTRAINT "FK_t_customer_mailbox" FOREIGN KEY (customer_id)
       REFERENCES ds.t_customer (customer_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+      ON UPDATE RESTRICT ON DELETE RESTRICT
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE ds.t_invitations
+ALTER TABLE ds.t_customer_mailbox
   OWNER TO postgres;
-GRANT ALL ON TABLE ds.t_invitations TO postgres;
-GRANT SELECT, UPDATE, INSERT ON TABLE ds.t_invitations TO ds_user;
+GRANT ALL ON TABLE ds.t_customer_mailbox TO postgres;
+GRANT SELECT, UPDATE, INSERT ON TABLE ds.t_customer_mailbox TO ds_user;
+
 
