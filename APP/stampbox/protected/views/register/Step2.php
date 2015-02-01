38bde97c54ae7dc5214e5dfc011450b5cee5bfe3
raulr <raulrebane71@gmail.com>
2015-02-01 23:25:33 +0200

    registration step2 design now ok

--- APP/stampbox/protected/views/register/Step2.php
+++ APP/stampbox/protected/views/register/Step2.php
@@ -4,17 +4,14 @@
 /* @var $form CActiveForm */
 
 $this->pageTitle=Yii::app()->name . ' - Register -> Step2';
+
 ?>
 
 <div class="col-md-6">
 <?php 
-    $form = $this->beginWidget('CActiveForm',
-    array(
-        'id' => 'Step2',
-        //'type'=>'horizontal',
-        'htmlOptions' => array('class'=>"form register", 'role'=>"form"), // for inset effect
-        )
-    ); 
+    echo '<br><h2>Incoming mail server</h2>';
+    
+    $form = $this->beginWidget('CActiveForm', array('id' => 'Step2', 'htmlOptions' => array('class'=>"form register", 'role'=>"form"))); 
 
     echo $form->hiddenField($model, 'maildomain');
     echo $form->hiddenField($model, 'mailtype');
@@ -28,13 +25,21 @@ $this->pageTitle=Yii::app()->name . ' - Register -> Step2';
     echo $form->numberField($model, 'incoming_port', array('class'=>'form-control', 'placeholder'=>'Port'));    
     
     echo $form->labelEx($model,'incoming_socket_type');
+    echo '<div class="select-style">';
     echo $form->dropDownList($model, 'incoming_socket_type',
               array('NULL'=>'None', 'ssl' => 'SSL', 'tls' => 'TLS'));
+    echo '</div>';
+    
+    $this->endWidget(); 
+    unset($form);    
 ?>
-    </div>
 </div>
 <div class="col-md-6 darker">
 <?php
+    echo '<br><h2>Outgoing mail server</h2>';
+
+    $form = $this->beginWidget('CActiveForm', array('id' => 'Step21', 'htmlOptions' => array('class'=>"form register", 'role'=>"form"))); 
+
     //echo $form->labelEx($model,'outgoing_hostname');
     echo $form->textField($model, 'outgoing_hostname', array('class'=>'form-control', 'placeholder'=>'e-mail server name'));
     echo $form->error($model, 'outgoing_hostname');
@@ -43,17 +48,12 @@ $this->pageTitle=Yii::app()->name . ' - Register -> Step2';
     echo $form->numberField($model, 'outgoing_port', array('class'=>'form-control', 'placeholder'=>'Port'));    
     
     echo $form->labelEx($model,'outgoing_socket_type');
+    echo '<div class="select-style">';
     echo $form->dropDownList($model, 'outgoing_socket_type',
               array('NULL'=>'None', 'ssl' => 'SSL', 'tls' => 'TLS'));
-?>
-    </div>
-</div>
-<button type="submit" class="btn btn-default"></button>
+    echo '</div><button type="submit" class="btn btn-default"></button></div>';
 
-<?php
 //echo Yii::app()->user->getFlash('info');
-
-$this->endWidget(); 
-unset($form);
-
+    $this->endWidget(); 
+    unset($form);    
 ?>
