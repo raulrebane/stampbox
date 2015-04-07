77372f27e26428869c5d14a8773f8742d5f142ea
raulr <raulrebane71@gmail.com>
2015-04-07 21:31:38 +0300

    Using bootstrap dialog instead of JQuery on whitelist multiple e-mail adding.

--- APP/stampbox/protected/views/whitelist/index.php
+++ APP/stampbox/protected/views/whitelist/index.php
@@ -28,24 +28,69 @@
         ?>
     </div>
     <div class="col-md-6 m-add">
-        <h1>Add from mailbox contacts</h1>
-        <?php $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
-        'id'=>'addFromContacts',
-        'options'=>array(
-            'title'=>'Add contacts to whitelist',
-            'width'=>800,
-            'height'=>1000,
-            'autoOpen'=>FALSE,
-            'resizable'=>true,
-            'modal'=>true,
-            'closeOnEscape'=>TRUE,
-            //'dialogClass'=>"no-close",
-            'overlay'=>array('backgroundColor'=>'#000','opacity'=>'0.8')
-            ),
-        ));         
-        $this->endWidget('zii.widgets.jui.CJuiDialog');
-        echo CHtml::button('Open Dialog', array('onclick'=>'$("#addFromContacts").dialog("open"); return false;',));
+        <?php
+            $form = $this->beginWidget('CActiveForm',array(
+                'id' => 'WhitelistD',
+                //'type'=>'horizontal',
+                'htmlOptions' => array('class'=>'form', 'role'=>'form'),
+                ));               
         ?>
+        <!-- Button trigger modal -->
+        <h1>Add from contacts</h1>
+        <button type="button" class="btn btn-primary btn-aqua" data-toggle="modal" data-target="#Whitelistdialog">
+            Add
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="Whitelistdialog" tabindex="-1" role="dialog" aria-labelledby="Whitelistlabel" 
+             aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="Whitelistlabel">Add mailbox contacts to whitelist</h4>
+            </div>
+            <div class="modal-body">
+                <?php
+                    $sort = new CSort();
+                    $sort->attributes = array('invited_email','name');
+                    $sort->defaultOrder=array('name'=>CSort::SORT_DESC);
+                    
+                    $contactDataProvider=new CActiveDataProvider('Invitations', array(
+                        'criteria'=>array('condition'=>'customer_id='.Yii::app()->user->getId()),
+                        'sort'=>$sort, 'pagination'=>array('pageSize'=>1000,)));
+                    $gridColumns = array(
+                        array(
+                            'id' => 'selectedIds',
+                            'class' => 'CCheckBoxColumn',
+                            'selectableRows'=>1000,
+                            'header'=>'Invite',
+                            'name'=>'invited_email',
+                            'disabled'=>function($data) {if ($data['invite']==='Y') return TRUE; else return FALSE;},
+                            //'checked'=>'($row<100 AND $data["invite"] <> "Y")'
+                            ),
+                        array('name'=>'name', 'header'=>'Name'),
+                        array('name'=>'invited_email', 'header'=>'E-mail'),
+                    );
+                    $this->widget('zii.widgets.grid.CGridView',array(
+                        'id'=>'invitation-grid',
+                        'enablePagination'=>FALSE,
+                        //'hideHeader'=>TRUE,
+                        'template' => '{items}',
+                        'htmlOptions'=>array('class'=>'content'),
+                        'dataProvider' => $contactDataProvider,
+                        'columns'=>$gridColumns
+                    )); 
+                ?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="submit" name="whitelistsubmit" class="btn btn-aqua">Add selected</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    <?php $this->endWidget(); ?>
     </div>
 </div>
     
