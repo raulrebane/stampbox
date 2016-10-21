b318e019ceb7c56f3dea33c263fc3a1475b10219
raulr <raulrebane71@gmail.com>
2016-10-21 09:58:06 +0300

    error testing

--- APP/stampbox/protected/controllers/UsermailboxController.php
+++ APP/stampbox/protected/controllers/UsermailboxController.php
@@ -90,8 +90,8 @@ class UsermailboxController extends Controller
                 $this->redirect(array('usermailbox/index'));
             }
             else {
-                list(, $model->maildomain) = explode("@", $model->useremail);
-                $model->registereddomain = mailconfig::model()->find('maildomain=:1', array(':1'=>$model->maildomain));
+                //list(, $model->maildomain) = explode("@", $model->useremail);
+                //$model->registereddomain = mailconfig::model()->find('maildomain=:1', array(':1'=>$model->maildomain));
                 $this->render('create',array('model'=>$model,));
                 Yii::app()->end();
             }
